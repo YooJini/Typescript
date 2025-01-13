ad12f133433ab13e53cfdae9b17a141fca37be91
Jini <wlsdl6685@gmail.com>
2025-01-13 14:18:59 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+function logName(name) {
+    console.log(name);
+}
+logName("yoo");
+var student = {
+    name: "jini",
+    course: "typescript",
+    grade: function () {
+        console.log("A");
+    },
+};
+var age = 20;
---
+++ app.ts
@@ -0,0 +1,15 @@
+function logName(name: string): void {
+  console.log(name);
+}
+
+logName("yoo");
+
+let student = {
+  name: "jini",
+  course: "typescript",
+  grade: function () {
+    console.log("A");
+  },
+};
+
+let age: number = 20;
